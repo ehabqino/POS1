d0f1e6c1914d3312940ea104b97b0a2debb9197f
ehab qino <ehabqino@gmail.com>
2021-07-15 07:40:53 +0300

    commit "test category model from pos viewMoedel"

--- src/js/viewModels/pos.js
+++ src/js/viewModels/pos.js
@@ -1,10 +1,16 @@
-define(['ojs/ojcore','knockout','jquery','ojs/ojbindingprovider'], 
-    function(oj,ko,$) {
+define(['ojs/ojcore','knockout','jquery','ojs/ojarraydataprovider','models/category.model','ojs/ojbindingprovider'], 
+    function(oj,ko,$,ArrayDataProvider,categoryModel) {
         function PosViewModel(){
             self = this;
             self.testData = ko.observableArray(["Ehab Test",'test2','test3','test4']);
             self.testColor = ko.observable(" oj-bg-danger-30");
             self.dataProvider = ko.observableArray(["category1","category2","category3","category4","category5","category6"]);
+            
+            categoryModel.getCategoryList((success,data)=>{
+                
+            });
+            
+            
         }
         return PosViewModel;
     
